9ac6639d3cfee9701dfd49bc8fe77a08306f53ab
kegma1 <kennetam02@gmai.com>
2022-09-19 12:13:40 +0200

    done

--- oblig2/pointPosition.py
+++ oblig2/pointPosition.py
@@ -10,21 +10,22 @@ def inRelationToLine(p0,p1,p2) -> str:
 
 
 
-x0=1
-y0=1
+x0 = 1
+y0 = 1
 x1 = 4
 y1 = 1
 x2 = 3
 y2 = 1
 
-# x0=1
-# y0=1
+# x0 = 1
+# y0 = 1
 # x1 = 3
 # y1 = 1
 # x2 = 4
 # y2 = 1
+
 p0 = (x0,y0)
 p1 = (x1,y1)
 p2 = (x2,y2)
 
-print(f"p2 is")
+print(f"p2 is on the {inRelationToLine(p0, p1, p2)} line")
