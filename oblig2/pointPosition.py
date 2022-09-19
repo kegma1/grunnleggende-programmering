a3264e997dc210f37b49e9a3ac4327745dc079ab
Kennet Mathisen <kennetam02@gmail.com>
2022-09-19 21:16:26 +0200

    fixed

--- oblig2/pointPosition.py
+++ oblig2/pointPosition.py
@@ -8,24 +8,13 @@ def inRelationToLine(p0,p1,p2) -> str:
     elif formula < 0:
         return "right side of the"
 
+p0Str = input("Enter a cord for p0. [1,1]: ")
+p1Str = input("Enter a cord for p1. [4,1]: ")
+p2Str = input("Enter a cord for p2. [3,1]: ")
 
+p0 = [int(x) for x in p0Str.split(",")]
+p1 = [int(x) for x in p1Str.split(",")]
+p2 = [int(x) for x in p2Str.split(",")]
 
-x0 = 1
-y0 = 1
-x1 = 4
-y1 = 1
-x2 = 3
-y2 = 1
-
-# x0 = 1
-# y0 = 1
-# x1 = 3
-# y1 = 1
-# x2 = 4
-# y2 = 1
-
-p0 = (x0,y0)
-p1 = (x1,y1)
-p2 = (x2,y2)
 
 print(f"p2 is on the {inRelationToLine(p0, p1, p2)} line")
