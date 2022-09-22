56f4a8012aa02067463664e57e926a03c22c2b74
kegma1 <kennetam02@gmai.com>
2022-09-22 10:01:39 +0200

    fixed shit

--- oblig2/pointPosition.py
+++ oblig2/pointPosition.py
@@ -2,11 +2,14 @@ def inRelationToLine(p0,p1,p2) -> str:
     formula = (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1])
 
     if formula > 0:
-        return "left side of the"
+        return "left side of the line"
     elif formula == 0:
-        return "same"
+        if p0[0] <= p2[0] <= p1[0] and p0[1] <= p2[1] <= p1[1]:
+            return "same line segment"
+        else:
+            return "same line"
     elif formula < 0:
-        return "right side of the"
+        return "right side of the line"
 
 p0Str = input("Enter a cord for p0. [1,1]: ")
 p1Str = input("Enter a cord for p1. [4,1]: ")
@@ -17,4 +20,4 @@ p1 = [int(x) for x in p1Str.split(",")]
 p2 = [int(x) for x in p2Str.split(",")]
 
 
-print(f"p2 is on the {inRelationToLine(p0, p1, p2)} line")
+print(f"p2 is on the {inRelationToLine(p0, p1, p2)}")
