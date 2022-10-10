0900bdf09c97f6c2f287e219700f4f666baf8967
kegma1 <kennetam02@gmai.com>
2022-10-10 12:19:52 +0200

    bruhhh

--- oblig2/listFormating.py
+++ oblig2/listFormating.py
@@ -7,11 +7,15 @@ def formatList(list) -> str:
     finalString = ", ".join(list[0:-1]) + finalString
     return finalString
 
-userList = []
-userInput = " "
+def main():
+    userList = []
+    userInput = " "
 
-while userInput != "":
-    userInput = input("Enter an item (blank to quit): ")
-    userList.append(userInput)
-userList.pop()
-print(f"The items are {formatList(userList)}.")
+    while userInput != "":
+        userInput = input("Enter an item (blank to quit): ")
+        userList.append(userInput)
+    userList.pop()
+    print(f"The items are {formatList(userList)}.")
+
+if __name__ == "__main__":
+    main()
