5c4fb6bca6f4baf021ca35da3b92a4697455fb11
kegma1 <kennetam02@gmai.com>
2022-10-13 13:11:07 +0200

    done

--- oblig4/radiobutton.py
+++ oblig4/radiobutton.py
@@ -1,18 +1,36 @@
 from tkinter import *
 
-def main():
-    main = Tk()
-    color = StringVar(main, "red")
-    Radiobutton(main, text="Red", variable=color, value="red").pack(row=0, column= 0)
-    Radiobutton(main, text="Yellow", variable=color, value="yellow").pack(row=0, column= 1)
-    Radiobutton(main, text="White", variable=color, value="white").pack(row=0, column= 2)
-    Radiobutton(main, text="Gray", variable=color, value="gray").pack(row=0, column= 3)
-    Radiobutton(main, text="Green", variable=color, value="green").pack(row=0, column= 4)
+main = Tk()
+main.geometry("280x126")
+main.resizable(0, 0)
+color = StringVar(main, "red")
 
-    text = Label(main, text="Welcome", bg=color.get())
-    text.pack(row=1)
+textFrame = Frame(main, bg="red")
+text = Label(textFrame, text="Welcome", bg="red")
 
-    mainloop()
+def onChange():
+    textFrame.config(bg=color.get())
+    text.config(bg=color.get())
 
-if __name__ == "__main__":
-    main()
+def move(x):
+    curr = int(text.place_info()["x"])
+    text.place_configure(x= curr + x)
+
+selector = Frame(main)
+selector.grid(row=0, column=3)
+Radiobutton(selector, text="Red", variable=color, value="red",command=onChange).grid(row=0, column=0)
+Radiobutton(selector, text="Yellow", variable=color, value="yellow",command=onChange).grid(row=0, column=1)
+Radiobutton(selector, text="White", variable=color, value="white",command=onChange).grid(row=0, column=2)
+Radiobutton(selector, text="Gray", variable=color, value="gray",command=onChange).grid(row=0, column=3)
+Radiobutton(selector, text="Green", variable=color, value="green",command=onChange).grid(row=0, column=4)
+
+textFrame.grid(row=1, column=3, sticky="WE", ipady=30, pady=5)
+text.place(width=280, y=17)
+
+buttons = Frame(main)
+buttons.grid(row=2, columnspan=5, sticky=S)
+
+Button(buttons, text="<-", command=lambda: move(-10)).grid(row=0,column=0)
+Button(buttons, text="->", command=lambda: move(10)).grid(row=0,column=1)
+
+main.mainloop()
