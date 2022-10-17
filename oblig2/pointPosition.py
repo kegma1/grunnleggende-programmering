a16a8513584df0f21765e938edd658bbb0547d92
kegma1 <kennetam02@gmai.com>
2022-10-17 10:25:40 +0200

    fikk -5 poeng fordi det ikke va 3 runksjoner

--- oblig2/pointPosition.py
+++ oblig2/pointPosition.py
@@ -1,15 +1,16 @@
-def inRelationToLine(p0,p1,p2) -> str:
+def rightOfTheLine(p0,p1,p2):
     formula = (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1])
+    return formula < 0
 
-    if formula > 0:
-        return "left side of the line"
-    elif formula == 0:
-        if p0[0] <= p2[0] <= p1[0] and p0[1] <= p2[1] <= p1[1]:
-            return "same line segment"
-        else:
-            return "same line"
-    elif formula < 0:
-        return "right side of the line"
+def leftOfTheLine(p0,p1,p2):
+    formula = (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1])
+    return formula > 0
+
+def onTheLine(p0,p1,p2):
+    formula = (p1[0] - p0[0]) * (p2[1] - p0[1]) - (p2[0] - p0[0]) * (p1[1] - p0[1])
+    return formula == 0
+
+
 
 p0Str = input("Enter a cord for p0. [1,1]: ")
 p1Str = input("Enter a cord for p1. [4,1]: ")
@@ -19,5 +20,12 @@ p0 = [int(x) for x in p0Str.split(",")]
 p1 = [int(x) for x in p1Str.split(",")]
 p2 = [int(x) for x in p2Str.split(",")]
 
-
-print(f"p2 is on the {inRelationToLine(p0, p1, p2)}")
+if leftOfTheLine(p0, p1, p2):
+    print("p2 is on the left side of the line")
+elif onTheLine(p0, p1, p2):
+    if p0[0] <= p2[0] <= p1[0] and p0[1] <= p2[1] <= p1[1]:
+        print("p2 is on the same line segment")
+    else:
+        print("p2 is on the same line")
+elif rightOfTheLine(p0, p1, p2):
+    print("p2 is on the right side of the line")
