48d44cf1e881c36402f656a0b4f661a90606170b
kegma1 <kennetam02@gmai.com>
2022-10-28 10:29:27 +0200

    oblig 5 🥳🥳🥳

---
+++ oblig5/finnGeneTest.py
@@ -0,0 +1,53 @@
+import unittest   # The test framework
+
+def findGenes(genome : str) -> list:
+    startSeq = "ATG"
+    stopSeqs = ["TAG", "TAA", "TGA"]
+    def findGene(seq: str) -> str | None:
+        possibleSeqs = [seq[i:i+3] for i in range(0, len(seq), 3)]
+        endIndex = 0
+        for i, seq in enumerate(possibleSeqs):
+            if seq == startSeq:
+                return None
+            if seq in stopSeqs:
+                endIndex = i
+                break
+        else: # We return none if we dont find an ending. the else branch after a for-loop only activate if the loop ends normally (not breaking).
+            return None
+        finalSeq = possibleSeqs[0:endIndex]
+        if len(finalSeq) == 0:
+            return
+        return "".join(finalSeq)
+    geneList = []
+    currentSeq = ""
+    i = 0
+    while i < len(genome):
+        currentSeq = genome[i:i+3]
+
+        if currentSeq == startSeq:
+            geneSeq = findGene(genome[i+3:len(genome)])
+            if geneSeq != None:
+                geneList.append(geneSeq)
+                i += len(geneSeq)
+        i += 1  
+    if len(geneList) == 0:
+        geneList.append("No genes found")
+    return geneList
+
+
+
+
+
+class Test_genome(unittest.TestCase):
+    def test_emty_string(self):
+        result = findGenes("")
+        self.assertEqual(result[0], "No genes found")    
+
+    def test_rubbish_string(self):
+        result = findGenes("I AM A STRING WITH NO GENES WHATSOEVER")
+        self.assertEqual(result[0], "No genes found")
+
+if __name__ == '__main__':
+
+    unittest.main()
+
